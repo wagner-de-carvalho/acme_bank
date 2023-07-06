e1ae03d9c7a568007a4bcfafa7ec66b537bf3132
wagner <wagncarv@outlook.com>
2023-07-05 21:07:18 -0300

    Testes Mox

--- test/acme_bank_web/controllers/users_controller_test.exs
+++ test/acme_bank_web/controllers/users_controller_test.exs
@@ -1,15 +1,36 @@
 defmodule AcmeBankWeb.UsersControllerTest do
   use AcmeBankWeb.ConnCase, async: true
+  import Mox
   alias AcmeBank.Users
+  alias AcmeBank.ViaCep.ClientMock
 
   setup do
+    # Mox
+    :verify_on_exit!
+
     user_params = %{"name" => "Acme Test", "email" => "acmetest@mail.com", "password" => "123456", "cep" => "29560000"}
     user_invalid_params = %{"name" => "Acme Test", "email" => "acmetest", "password" => "123456", "cep" => "1234567"}
-    %{user_params: user_params, user_invalid_params: user_invalid_params}
+
+    body = %{
+      "bairro" => "",
+      "cep" => "29460-000",
+      "complemento" => "",
+      "ddd" => "28",
+      "gia" => "",
+      "ibge" => "3201100",
+      "localidade" => "Bom Jesus do Norte",
+      "logradouro" => "",
+      "siafi" => "5621",
+      "uf" => "ES"
+    }
+
+    %{user_params: user_params, user_invalid_params: user_invalid_params, body: body}
   end
 
   describe "create/2" do
-    test "creates an user successfully", %{conn: conn, user_params: params} do
+    test "creates an user successfully", %{conn: conn, user_params: params, body: body} do
+      expect(ClientMock, :call, fn _cep -> {:ok, body} end)
+
       response =
         conn
         |> post(~p"/api/users", params)
@@ -22,6 +43,8 @@ defmodule AcmeBankWeb.UsersControllerTest do
     end
 
     test "returns an error when there are invalid params", %{conn: conn, user_invalid_params: params} do
+      expect(ClientMock, :call, fn _cep -> {:error, :bad_request} end)
+
       response =
         conn
         |> post(~p"/api/users", params)
@@ -32,7 +55,8 @@ defmodule AcmeBankWeb.UsersControllerTest do
   end
 
   describe "delete/2" do
-    test "successfully deletes an user", %{conn: conn, user_params: params} do
+    test "successfully deletes an user", %{conn: conn, body: body, user_params: params} do
+      expect(ClientMock, :call, fn _cep -> {:ok, body} end)
       {:ok, user} = Users.create(params)
 
       conn = conn |> delete(~p"/api/users/#{user.id}")
